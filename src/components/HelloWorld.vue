46c4735f0f11f382984eee08cebb20f6afe85320
Carl Gray <carl@firstfnf.com>
2019-02-27 16:03:30 +1100

    clickable progress bar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,8 @@
    <div><audio :ontimeupdate.prop="updateProgress" src="http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3" id="player"></audio>  
 <button @click="play">Play</button>
 <button @click="pause">Pause</button>
-<progress id="seekbar" value="0" max="1" style="width:400px;"></progress></div>
+
+<progress @click="progressClick" id="seekbar" value="0" max="1" style="width:400px;"></progress></div>
 <div class="flex1"><div>{{ currentTime  }}</div><div> /  {{ duration }}</div></div>
   </div>
 </template>
@@ -27,6 +28,24 @@ export default {
 
     pause() {
     document.getElementById('player').pause();
+},
+    skipTime() {
+      var player = document.getElementById('player')
+
+      player.currentTime = 20;
+},
+
+    progressClick(e) {
+      let el = document.getElementById('seekbar');
+      var x = e.pageX - el.offsetLeft;
+      var startPos = document.getElementById('seekbar').position;
+      var xconvert = x/400;
+      var finalx = (xconvert).toFixed(1);
+      document.getElementById('seekbar').value = finalx
+     
+      var player = document.getElementById('player');
+      var finalseconds = xconvert*player.duration;
+      player.currentTime = finalseconds;
 },
      updateProgress() {
     var player = document.getElementById('player');
@@ -69,13 +88,19 @@ progress[value] {
 progress[value]::-webkit-progress-bar {
   background-color: rgba(252, 70, 4, 0.0);
   border-radius: 2px;
+  
   /* box-shadow: 0 2px 5px rgba(0, 0, 0, 0.0) inset; */
 }
 /* add funky background */
 progress[value]::-webkit-progress-value {
   background-color: rgba(252, 70, 4, 0.4);
 
-    border-radius: 2px; 
+border-right:2px solid red;
+  -moz-box-sizing:border-box;
+  -webkit-box-sizing:border-box;
+  box-sizing:border-box;
+
+    border-radius: 1px; 
     background-size: 35px 20px, 100% 100%, 100% 100%;
 }
 </style>
