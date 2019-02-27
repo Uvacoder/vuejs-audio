91b576ffba206a21fd50e61d4f5f2cdd3e0e05bf
Carl Gray <carl@firstfnf.com>
2019-02-27 14:29:01 +1100

    scrub bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Audio Player"/>
   </div>
 </template>
 
@@ -23,6 +22,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover;
+  /* margin-top: 60px; */
+  background-image: url("./assets/alps-background.jpg")
+  
+
+
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <div><audio :ontimeupdate.prop="updateProgress" src="http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3" id="player"></audio>  
+<button @click="play">Play</button>
+<button @click="pause">Pause</button>
+<progress id="seekbar" value="0" max="1" style="width:400px;"></progress></div>
+<div class="flex1"><div>{{ currentTime  }}</div><div> /  {{ duration }}</div></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data: function() {
+    return {
+      currentTime: null,
+      duration: null
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    play() {
+    document.getElementById('player').play();
+},
+
+    pause() {
+    document.getElementById('player').pause();
+},
+     updateProgress() {
+    var player = document.getElementById('player');
+    var progressbar = document.getElementById('seekbar');
+    progressbar.value = (player.currentTime / player.duration);
+    // convert time
+     const convertTime = (setTime) => {
+    var date = new Date(null);
+    date.setSeconds(setTime);
+    var utc = date.toUTCString();
+    return utc.substr(utc.indexOf(':') - 2, 8);
+    }
+    this.currentTime = convertTime(player.currentTime);
+    this.duration = convertTime(player.duration)
+}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+/* .hello {
+background-image: url("../assets/alps-background.jpg")
+} */
+
+.flex1 {
+  display: flex;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+progress[value] {
+  /* Reset the default appearance */
+  -webkit-appearance: none;
+   appearance: none;
+   
+
+  width: 250px;
+  height: 100vh;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+/* add box shadow */
+progress[value]::-webkit-progress-bar {
+  background-color: rgba(252, 70, 4, 0.0);
+  border-radius: 2px;
+  /* box-shadow: 0 2px 5px rgba(0, 0, 0, 0.0) inset; */
 }
-a {
-  color: #42b983;
+/* add funky background */
+progress[value]::-webkit-progress-value {
+  background-color: rgba(252, 70, 4, 0.4);
+
+    border-radius: 2px; 
+    background-size: 35px 20px, 100% 100%, 100% 100%;
 }
 </style>
