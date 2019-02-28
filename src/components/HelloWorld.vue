0b965a8683ed37d28ae760b771822e585fb281d7
Carl Gray <carl@firstfnf.com>
2019-02-28 16:20:52 +1100

    audio visuals with scrub

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="hello">
-   <div><audio :ontimeupdate.prop="updateProgress" src="http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3" id="player"></audio>  
+    <div><button @click="audioVisual">TRYING TO GET DATA FROM AUDIO VISUAL INPUT</button>
+    </div>
+    <div class="allboxes">
+    <div class="changeheight0" :style="{'height':data1[0]+'px'}"><p></p></div>
+    <div class="changeheight1" :style="{'height':data1[1]+'px'}"><p></p></div>
+    <div class="changeheight2" :style="{'height':data1[2]+'px'}"><p></p></div>
+    <div class="changeheight3" :style="{'height':data1[3]+'px'}"><p></p></div>
+    <div class="changeheight4" :style="{'height':data1[4]+'px'}"><p></p></div>
+    <div class="changeheight5" :style="{'height':data1[5]+'px'}"><p></p></div>
+    <div class="changeheight6" :style="{'height':data1[6]+'px'}"><p></p></div>
+    <div class="changeheight7" :style="{'height':data1[7]+'px'}"><p></p></div>
+    <!-- <div class="changeheight8" :style="{'height':data1[8]+'px'}"><p></p></div> -->
+    </div>
+   <div><audio :ontimeupdate.prop="updateProgress" id="player"></audio> 
+   <div class="float"> 
 <button @click="play">Play</button>
 <button @click="pause">Pause</button>
 
-<progress @click="progressClick" id="seekbar" value="0.2" max="1" style="width:400px;"></progress></div>
+<progress @click="progressClick" id="seekbar" value="0.2" max="1" style="width:600px;"></progress></div>
 <div class="flex1"><div>{{ currentTime  }}</div><div> /  {{ duration }}</div></div>
+</div>
   </div>
 </template>
 
@@ -15,7 +30,8 @@ export default {
   data: function() {
     return {
       currentTime: null,
-      duration: null
+      duration: null,
+      data1: [0, 0, 0, 0, 0, 0, 0, 0]
     }
   },
   props: {
@@ -30,11 +46,65 @@ export default {
     document.getElementById('player').pause();
 },
 
+  audioVisual() {
+    const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
+
+    var audioElement = document.getElementById('player');
+    audioElement.src = `https://api.soundcloud.com/tracks/575588511/stream?client_id=${CLIENT_ID}`
+    // audioElement.src = require('../assets/SampleAudio.mp3');
+    audioElement.crossOrigin = "anonymous";
+
+    var ctx = new AudioContext();
+    var sourceNode = ctx.createMediaElementSource(audioElement);
+
+    var analyser = ctx.createAnalyser();
+    sourceNode.connect(analyser);
+    analyser.connect(ctx.destination);
+
+    audioElement.play()
+
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+    analyser.getByteFrequencyData(frequencyData);
+
+    this.data1 = frequencyData;
+
+    // console.log(frequencyData);
+
+
+
+    
+
+
+
+      setInterval(function() {
+        analyser.getByteFrequencyData(frequencyData);
+
+        function groupAverage(arr, n) {
+      var result = [];
+      for (var i = 0; i < arr.length;) {
+       var sum = 0;
+       for(var j = 0; j< n; j++){
+      // Check if value is numeric. If not use default value as 0
+        sum += +arr[i++] || 0
+       }
+       result.push(sum/n);
+     }
+       return result
+      }
+        let result = groupAverage(frequencyData, 128)
+
+        this.data1 = result;
+      }, 500);
+
+
+
+},
     progressClick(e) {
       let el = document.getElementById('seekbar');
       var x = e.pageX - el.offsetLeft;
       var startPos = document.getElementById('seekbar').position;
-      var xconvert = x/400;
+      var xconvert = x/600;
       var finalx = (xconvert).toFixed(1);
       document.getElementById('seekbar').value = finalx
      
@@ -62,10 +132,99 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .hello {
-background-image: url("../assets/alps-background.jpg")
+
+.allboxes {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+}
+
+.changeheight0 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight1 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight2 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight3 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight4 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight5 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight6 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+.changeheight7 {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 20px;
+  justify-items: center;
+}
+
+/* .changeheight8 {
+  display: flex;
+  background-color: #5000FF;
+  color: white;
+  width: 20px;
+  justify-items: center;
 } */
 
+.float {
+  position: fixed;
+  top: 0;
+  float:initial;
+}
 .flex1 {
   display: flex;
   justify-content: center;
@@ -76,7 +235,7 @@ progress[value] {
    appearance: none;
    
 
-  width: 250px;
+  /* width: 250px; */
   height: 100vh;
 }
 /* add box shadow */
